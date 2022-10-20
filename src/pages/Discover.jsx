34b4c938971527b02ac52ede0e4864c0a137ce07
Mateusnasciment <mateus.nasciment@hotmail.com>
2022-10-20 02:00:03 +0000

    add-component-geners

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,13 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from '../components';
+
+import { geners }  from '../assets/constants';
+// Os Geners irei importar um array com title,categoria e valor
+const Discover = () => {
+        return (
+            <div> 
+                Discover
+            </div>
+        )
+}
 
 export default Discover;
