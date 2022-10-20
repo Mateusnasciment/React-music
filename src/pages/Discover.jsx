aaaad879822d7eddb0ccdf3380e243880abcd955
Mateusnasciment <mateus.nasciment@hotmail.com>
2022-10-20 18:58:14 +0000

    0.0.3

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,13 +1,24 @@
 import { Error, Loader, SongCard } from '../components';
-
-import { geners }  from '../assets/constants';
+import { genres } from '../assets/constants';
 // Os Geners irei importar um array com title,categoria e valor
+
 const Discover = () => {
-        return (
-            <div> 
-                Discover
+    console.log(genres);
+    return(
+        <div className="flex flex-col"> 
+            <div className="w-full flex justify-between items-center
+            sm:flex-row flex-col mt-4 mb-10">
+                <div className="font-bold text-3x1 text-white">
+                    <h2>Discover</h2>
+                </div>
+                <select
+                    onChange={() => {}}
+                    value=""
+                    ClassName="bg-black text-gray-300"
+                />
             </div>
+        </div>
         )
-}
+    }
 
 export default Discover;
