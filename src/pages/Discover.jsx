8a5a34e4a1d96522cfb6d7b4ef3a479bbc064d68
Mateusnasciment <mateus.nasciment@hotmail.com>
2022-10-23 01:11:11 +0000

    0.0.4

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -3,14 +3,12 @@ import { genres } from '../assets/constants';
 // Os Geners irei importar um array com title,categoria e valor
 
 const Discover = () => {
-    console.log(genres);
+    const genresTitle = 'Pop';
     return(
         <div className="flex flex-col"> 
             <div className="w-full flex justify-between items-center
             sm:flex-row flex-col mt-4 mb-10">
-                <div className="font-bold text-3x1 text-white">
-                    <h2>Discover</h2>
-                </div>
+                    <h2 ClassName="font-bold text-3x1 text-white text-left">Discover {genresTitle}</h2>
                 <select
                     onChange={() => {}}
                     value=""
